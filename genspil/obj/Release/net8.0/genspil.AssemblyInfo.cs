a923bbb3ef82d0e6055cb627bf8995e8092e6cca
markus <modj62361@edu.ucl.dk>
2024-03-26 16:12:20 +0100

    team 4

--- genspil/obj/Release/net8.0/genspil.AssemblyInfo.cs
+++ genspil/obj/Release/net8.0/genspil.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("genspil")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea8ba6b3cb2b6ef764f88fb0f62897f31f43d7e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b1013b4601304ce36fc8064008a3826fbfced5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("genspil")]
 [assembly: System.Reflection.AssemblyTitleAttribute("genspil")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
