c9010b98aa8f054f133acb95b1ad6040dec9496f
FjamZoo <158572847+lwhp@users.noreply.github.com>
2024-04-06 23:33:24 +0200

    Use primary constructor

--- genspil/Boardgame.cs
+++ genspil/Boardgame.cs
@@ -6,30 +6,19 @@ using System.Threading.Tasks;
 
 namespace genspil
 {
-    internal class Boardgame
+    internal class Boardgame(string name, string genre, int minPlayers, int maxPlayers, Condition conditions, float price, string boardgameID)
     {
-        public Boardgame(string name, string genre, int minPlayers, int maxPlayers, Condition conditions, float price, string boardgameID)
-        {
-            Name = name;
-            Genre = genre;
-            MinPlayers = minPlayers;
-            MaxPlayers = maxPlayers;
-            Conditions = conditions;
-            Price = price;
-            BoardgameID = boardgameID;
-            
-        }
+        public string Name { get; set; } = name;
+        public string Genre { get; set; } = genre;
+        public int MinPlayers { get; set; } = minPlayers;
+        public int MaxPlayers { get; set; } = maxPlayers;
+        public Condition Conditions { get; set; } = conditions;
+        public float Price { get; set; } = price;
+        public string BoardgameID { get; set; } = boardgameID;
 
-        public string Name { get; set; }
-        public string Genre { get; set; }
-        public int MinPlayers { get; set; }
-        public int MaxPlayers { get; set; }
-        public Condition Conditions { get; set; }
-        public float Price { get; set; }
-        public string BoardgameID { get; set; }
-        
-       
-    }public enum Condition 
+
+    }
+    public enum Condition 
     {
         New,
         Used,
